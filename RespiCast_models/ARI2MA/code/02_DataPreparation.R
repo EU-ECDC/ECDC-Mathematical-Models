b03ed0269f84a62efab86e556a5e6537d856569a
Lydia Champezou <51203703+LydiaChamp@users.noreply.github.com>
2024-03-25 13:55:28 +0100

    Update 02_DataPreparation.R

--- RespiCast_models/ARI2MA/code/02_DataPreparation.R
+++ RespiCast_models/ARI2MA/code/02_DataPreparation.R
@@ -1,3 +1,9 @@
+########################################
+### Author: Lydia Champezou
+### Date: December 2023
+### Last update: March 2024
+########################################
+
 # ---- Keep the countries that report ----
 max_date <- max(data_cases$truth_date)
 

8a7a9e84fd994574c95ea065a21d3d8874109a63
Rok Grah <Rok.Grah@ecdc.europa.eu>
2024-09-27 11:12:03 +0200

    Bug fixes

--- Forecasting-hubs_models/models/Lydia-SARIMA/code/05_FinalOutputCSV.R
+++ Forecasting-hubs_models/models/Lydia-SARIMA/code/05_FinalOutputCSV.R
@@ -26,21 +26,35 @@ final_output <- function(monday_date,run_COVID_cases,run_COVID_deaths, run_COVID
   # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   # Dataframe with results [format like the upload to GitHub]
   for (target0 in c("case","hosp","death")){
-    x = Lydia_ARI2MA %>% filter(grepl(target0,target))
+    x = Lydia_SARIMA %>% filter(grepl(target0,target))
 
     # Make sure 'value' is double
     x$value = as.double(x$value)
     
     # Prepare the dataframe to be used in the 'plot_step_ahead_model_output' function below
     plot_mod_log = x %>% 
-      mutate(model_id="log",
-             output_type_id = as.numeric(output_type_id)) %>% 
-      rename(target_date=target_end_date) %>% 
-      filter(output_type != "median")
+      mutate(model_id="log") %>%
+      mutate(quantile = as.character(quantile)) %>%
+      rename(target_date=target_end_date,
+             output_type_id = quantile,
+             output_type = type) %>% 
+      filter(output_type != "point")
+    
+    # Reported data - prepare for plotting
+    if (target == "case"){
+      df_data = data_cases
+    } else if (target == "hosp") {
+      df_data = data_hosps
+    } else if (target == "death") {
+      df_data = data_deaths
+    } else {
+      message("Plotting function: Wrong target name")
+    }
+    df_data = df_data %>% rename(observation = value) # correct column names
     
     # Plot the figure
-    fig = plot_step_ahead_model_output(plot_mod_log, # Forecasts
-                                       df_train %>% mutate(time_idx=date) %>% filter(date>ymd("2024-01-01")), # Reported data
+    fig = plot_step_ahead_model_output( as_tibble(plot_mod_log), # Forecasts
+                                       df_data %>% mutate(time_idx=date) %>% filter(date>ymd("2024-01-01")), # Reported data
                                        facet=c("location"), facet_scales = "free",
                                        #intervals = c(0.95),
                                        interactive=F)
